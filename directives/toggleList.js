b4b0045ef3c144c82a56e7a1794ca341266bde2d
ahmed gaber <ahmedig22@gmail.com>
2017-11-27 13:09:50 +0200

    togg

--- directives/toggleList.js
+++ directives/toggleList.js
@@ -1,4 +1,4 @@
-applist.directive('headingList', function(){
+applist.directive('toggleList', function(){
     return {
         replace:true,
         template: '<div ng-class="sample-show-hide"><h4 class="panel panel-danger sample-show-hide" ng-click="ToggleVisible()">Toggle Me</h4><div ng-show="visible" ng-transclude></div></div>',
